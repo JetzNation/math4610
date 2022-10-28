84e7eef32dbce0379b9b67f76d27c83530ef190b
JetzNation <andyskabelund2@gmail.com>
2022-10-28 13:08:55 -0600

    Update secant.py

--- Python Code/Root Finding/secant.py
+++ Python Code/Root Finding/secant.py
@@ -6,15 +6,18 @@ def f_of_x(x):
 
 def secantMethod(func, x0, x1, maxIter):
     iteration = 0
+    tol = .00000001
+    error = 10 * tol
 
-    while (iteration < maxIter):
+    while (iteration < maxIter and error > tol):
         fx0 = f_of_x(x0)
         fx1 = f_of_x(x1)
-
-        xi = x0 - (fx0 / (fx0 - fx1))
+        xi = x1 - (fx1 * (x1 - x0)) / (fx1 - fx0)
+        error = np.abs(xi - x1)
         x0 = x1
         x1 = xi
+        fx0 = fx1
         iteration += 1
-    print(f"The root is found at {xi} after {iteration} iterations")
+    print(f"The root is found at {x1} after {iteration} iterations")
 
-secantMethod(f_of_x, 3, 5, 5)
+secantMethod(f_of_x, -1, 1, 25)
